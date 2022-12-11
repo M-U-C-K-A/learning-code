ee28f5c89db80bac2611e0f2a4eab6ffe24cd7f1
Mucka <96072629+M-U-C-K-A@users.noreply.github.com>
2022-12-11 22:26:20 +0100

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -32,72 +32,80 @@ client.on('message', message => {
 
                 // On utilise une fonction asynchrone pour récupérer le prix de la crypto-monnaie.
                 (async function _iife() {
-                    try {
-                        // On récupère les valeurs de l'objet retourné par l'API Binance et on les stocke dans le tableau "arrayPrice".
-                        arrayPrice = Object.values(await tiny.get({
-                            url
-                        }));
+                            try {
+                                // On récupère les valeurs de l'objet retourné par l'API Binance et on les stocke dans le tableau "arrayPrice".
+                                arrayPrice = Object.values(await tiny.get({
+                                    url
+                                }));
 
-                        // On récupère le prix de la crypto-monnaie à partir des informations stockées dans le tableau "arrayPrice" et on le stocke dans la variable "price".
-                        price = arrayPrice[0]['price'];
+                                // On récupère le prix de la crypto-monnaie à partir des informations stockées dans le tableau "arrayPrice" et on le stocke dans la variable "price".
+                                price = arrayPrice[0]['price'];
 
-                        // On envoie un message dans le chat// Récupère le premier élément du tableau de prix
-                        const price = arrayPrice[0]['price'];
+                                // On envoie un message dans le chat// Récupère le premier élément du tableau de prix
+                                const price = arrayPrice[0]['price'];
 
-                        // Envoie un message dans le canal avec un embed contenant les informations sur l'ordre LONG
-                        message.channel.send({
-                            embed: {
-                                color: 3447003, // Code couleur
-                                author: { // Auteur du message
-                                    name: message.author.username, // Nom d'utilisateur
-                                    icon_url: message.author.displayAvatarURL() // Icône d'utilisateur
-                                },
-                                description: message.author.username + " a ouvert un LONG " + args[0], // Description du message
-                                fields: [ // Champs d'informations supplémentaires
-                                    {
-                                        name: "Entry",
-                                        value: price + "$",
-                                        inline: false
-                                    }, // Prix d'entrée
-                                    {
-                                        name: "SL",
-                                        value: args[4] + "$",
-                                        inline: true
-                                    }, // Stop loss
-                                    {
-                                        name: "TP",
-                                        value: args[6] + "$",
-                                        inline: true
-                                    }, // Take profit
-                                    {
-                                        name: "Leverage",
-                                        value: args[7],
-                                        inline: true
-                                    } // Levier
-                                ],
-                            }
-                        });
+                                // Envoie un message dans le canal avec un embed contenant les informations sur l'ordre LONG
+                                message.channel.send({
+                                    embed: {
+                                        color: 3447003, // Code couleur
+                                        author: { // Auteur du message
+                                            name: message.author.username, // Nom d'utilisateur
+                                            icon_url: message.author.displayAvatarURL() // Icône d'utilisateur
+                                        },
+                                        description: message.author.username + " a ouvert un LONG " + args[0], // Description du message
+                                        fields: [ // Champs d'informations supplémentaires
+                                            {
+                                                name: "Entry",
+                                                value: price + "$",
+                                                inline: false
+                                            }, // Prix d'entrée
+                                            {
+                                                name: "SL",
+                                                value: args[4] + "$",
+                                                inline: true
+                                            }, // Stop loss
+                                            {
+                                                name: "TP",
+                                                value: args[6] + "$",
+                                                inline: true
+                                            }, // Take profit
+                                            {
+                                                name: "Leverage",
+                                                value: args[7],
+                                                inline: true
+                                            } // Levier
+                                        ],
+                                    }
+                                });
 
-                        // Crée un nouvel objet "Calls" contenant les informations sur l'ordre LONG
-                        const calls = new Calls({
-                            username: message.author.username, // Nom d'utilisateur
-                            userID: message.author.id, // ID d'utilisateur
-                            callType: 'LONG', // Type d'ordre (LONG)
-                            coin: args[0], // Monnaie
-                            valueEntry: price, // Prix d'entrée
-                            entryTime: message.createdAt, // Date/heure d'ouverture
-                            tp: args[6], // Take profit
-                            sl: args[4], // Stop loss
-                            closeTime: '', // Date/heure de fermeture (vide pour l'instant)
-                            valueClose: '' // Valeur de fermeture (vide pour l'instant)
-                        });
-
-                        // Sauvegarde l'objet "Calls" en base de données
-                        calls.save()
-                            .then(result => console.log(result))
-                            .catch(err => console.log(err));
-                    } catch (err) {
-                        console.log('ruh roh!', err)
-                    }
-                })();
-            }
+                                // Crée un nouvel objet "Calls" contenant les informations sur l'ordre LONG
+                                const calls = new Calls({
+                                    username: message.author.username, // Nom d'utilisateur
+                                    userID: message.author.id, // ID d'utilisateur
+                                    callType: 'LONG', // Type d'ordre (LONG)
+                                    coin: args[0], // Monnaie
+                                    valueEntry: price, // Prix d'entrée
+                                    entryTime: message.createdAt, // Date/heure d'ouverture
+                                    tp: args[6], // Take profit
+                                    sl: args[4], // Stop loss
+                                    closeTime: '', // Date/heure de fermeture (vide pour l'instant)
+                                    valueClose: '' // Valeur de fermeture (vide pour l'instant)
+                                });
+                                // Définition de la fonction asynchrone
+                                (async function() {
+                                    try {
+                                        // Tentative de sauvegarde de l'objet "Calls" en base de données
+                                        await calls.save()
+                                            .then(result => {
+                                                // Si la sauvegarde réussit, affiche le résultat dans la console
+                                                console.log(result);
+                                            })
+                                            .catch(err => {
+                                                // Si la sauvegarde échoue, affiche l'erreur dans la console
+                                                console.log(err);
+                                            });
+                                    } catch (err) {
+                                        // Si une erreur se produit dans le bloc try/catch, affiche un message d'erreur dans la console
+                                        console.log('ruh roh!', err)
+                                    }
+                                })();
