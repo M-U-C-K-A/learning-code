050aa3895c11494b3f5af3a8238ee8372d5c280c
Mucka <96072629+M-U-C-K-A@users.noreply.github.com>
2022-12-11 16:25:48 +0100

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -40,3 +40,42 @@ client.on('message', message => {
         price = arrayPrice[0]['price'];
 
         // On envoie un message dans le chat
+
+                        price = arrayPrice[0]['price'];
+
+                        message.channel.send({
+                            embed: {
+                                color: 3447003,
+                                author: {
+                                    name: message.author.username,
+                                    icon_url: message.author.displayAvatarURL()
+                                },
+                                description: message.author.username + " a ouvert un LONG " + args[0],
+                                fields: [
+                                    { name: "Entry", value: price + "$", inline: false },
+                                    { name: "SL", value: args[4] + "$", inline: true },
+                                    { name: "TP", value: args[6] + "$", inline: true },
+                                    { name: "Leverage", value: args[7], inline: true }
+                                ],
+                            }
+                        });
+                        const calls = new Calls({
+                            username: message.author.username,
+                            userID: message.author.id,
+                            callType: 'LONG',
+                            coin: args[0],
+                            valueEntry: price,
+                            entryTime: message.createdAt,
+                            tp: args[6],
+                            sl: args[4],
+                            closeTime: '',
+                            valueClose: ''
+                        });
+                        calls.save()
+                            .then(result => console.log(result))
+                            .catch(err => console.log(err));
+                    } catch (err) {
+                        console.log('ruh roh!', err)
+                    }
+                })();
+            }
