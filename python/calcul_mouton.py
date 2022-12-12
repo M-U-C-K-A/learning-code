2b56f918bb02bba1bb0deef0ebf719a1fdbe3871
Mucka <96072629+M-U-C-K-A@users.noreply.github.com>
2022-12-12 20:35:37 +0100

    Update calcul_mouton.py

--- python/calcul_mouton.py
+++ python/calcul_mouton.py
@@ -9,3 +9,6 @@ for i in range(34):
     print(rouge+vert,'  ',bleu)
     total += bleu+rouge+vert
 print(total+2)
+#Ce code permet de calculer le nombre total de bleus, rouges et verts dans une série de 34 itérations. 
+#La boucle for parcourt les 34 itérations, en utilisant les valeurs des couleurs précédentes pour calculer le nombre de bleus, rouges et verts pour chaque itération. 
+#Le nombre total de chaque couleur est ensuite ajouté au total global, puis le résultat final est affiché en ajoutant les 2 bleus initiaux.
